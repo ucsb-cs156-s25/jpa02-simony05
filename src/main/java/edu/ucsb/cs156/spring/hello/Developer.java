d757a75637713f4223265ccebf615985ffd7b258
Simon <simonryan@ucsb.edu>
2025-04-03 18:04:34 -0700

    xy - updated with actual developer name

--- src/main/java/edu/ucsb/cs156/spring/hello/Developer.java
+++ src/main/java/edu/ucsb/cs156/spring/hello/Developer.java
@@ -16,11 +16,7 @@ public class Developer {
      */
 
     public static String getName() {
-        // TODO: Change this to your name
-        // You may use just the name that is used on <https://bit.ly/cs156-s25-teams>
-        // i.e. your first name, or your first and initial of last name
-
-        return "Chris G.";
+        return "Simon";
     }
 
     /**
@@ -30,7 +26,7 @@ public class Developer {
 
     public static String getGithubId() {
         // TODO: Change this to your github id
-        return "cgaucho";
+        return "simony05";
     }
 
     /**
@@ -40,13 +36,12 @@ public class Developer {
     
     public static Team getTeam() {
         // TODO: Change this to your team name
-        Team team = new Team("s25-xx");
-        team.addMember("Alice");
-        team.addMember("Bob");
-        team.addMember("Chris G.");
-        team.addMember("Danny");
-        team.addMember("Eve");
-        team.addMember("Frances");
+        Team team = new Team("s25-12");
+        team.addMember("Arthur");
+        team.addMember("Michael");
+        team.addMember("Shawn");
+        team.addMember("Jay");
+        team.addMember("Hung");
         return team;
     }
 }
